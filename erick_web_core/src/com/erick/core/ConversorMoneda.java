f87c53487294457d37d5348b9c5cab173ed73527
ErickRivera <110272462+ErickRiver@users.noreply.github.com>
2023-09-15 15:58:26 -0600

    Add files via upload

---
+++ erick_web_core/src/com/erick/core/ConversorMoneda.java
@@ -0,0 +1,9 @@
+package com.erick.core;
+
+public class ConversorMoneda {
+    
+    public double convertirCantidad(double precioDivisa, double cantidad) {   
+        return precioDivisa * cantidad;
+    }
+    
+}
